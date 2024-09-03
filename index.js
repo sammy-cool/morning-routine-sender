317bc3265cdf8ad2e3ed5fed3824632a10e29466
Priyanshu Patel <priyanshu.alt191@gmail.com>
2024-09-03 18:15:38 +0530

    updated script

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { sendEmail } = require("./scheduled-jobs/email-jobs");
 require("dotenv").config();
 const cron = require("node-cron");
-const compression = require('compression');
+const compression = require("compression");
 
 // Express app
 const app = express();
@@ -12,13 +12,29 @@ const port = process.env.PORT || 3000;
 // Default Message for root URL
 app.get("/", (req, res) => {
   const domain = req.protocol + "://" + req.get("host");
-  console.log('Domain:', domain);
+  console.log("Domain:", domain);
   const endpointLink = `${domain}/send-email`;
-
   res.send(`
-    Hit The Send Email Endpoint to receive the email!<br>
-    <a href="${endpointLink}" target="_blank">Send Email</a>
+    <html>
+      <head>
+        <title>Send Email</title>
+      </head>
+      <body>
+        <h1>Hit The Send Email Endpoint to receive the email!</h1>
+        <a href="${endpointLink}" target="_blank">Send Email</a>
+      </body>
+    </html>
   `);
+});
+
+// Endpoint to send an email
+app.get("/send-email", async (req, res) => {
+  try {
+    sendEmail(); // Wait for the sendEmail function to complete
+    res.send("Email sent successfully!");
+  } catch (error) {
+    res.status(500).send("Error sending email: " + error.toString());
+  }
 });
 
 // Example Route
@@ -28,16 +44,16 @@ app.get("/example-route", (req, res, next) => {
 });
 
 // Schedule the email to be sent daily at 6:55 AM
-cron.schedule('55 6 * * *', () => {
-  console.log('Running a task every day at 6:55 AM');
+cron.schedule("55 6 * * *", () => {
+  console.log("Running a task every day at 6:55 AM");
   sendEmail();
 });
 
 //! Schedule a job to run every minute testing purpose only
 // cron.schedule('* * * * *', () => {
 //   console.log('Running a task every minute');
-  // Add logic here to check if the current time is 30 seconds past the minute mark
-  // For testing purposes, you might use setTimeout within this job
+// Add logic here to check if the current time is 30 seconds past the minute mark
+// For testing purposes, you might use setTimeout within this job
 //   setTimeout(() => {
 //     console.log('Running a test task 30 seconds after the minute');
 //     sendEmail();
