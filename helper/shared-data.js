49166e7848c32d3417f20ff55f83cbbfb1d37adf
Priyanshu Patel <priyanshu.alt191@gmail.com>
2024-09-03 16:13:00 +0530

    minor enhancement changes for email html-template
    minor enhancement changes for email html-template

--- helper/shared-data.js
+++ helper/shared-data.js
@@ -1,5 +1,8 @@
 // shared-data.js
 const crypto = require("crypto");
+const fs = require("fs");
+const path = require("path");
+
 let cache = new Map();
 
 function updateCache(key, value) {
@@ -12,7 +15,9 @@ function getCacheValue(key) {
   return cache.get(key);
 }
 
-function getNewRandomQuote(lastSentQuote = getCacheValue("lastSentQuote") || "") {
+function getNewRandomQuote(
+  lastSentQuote = getCacheValue("lastSentQuote") || ""
+) {
   const quotes = [
     "Rise and shine.",
     "Smile, it's a new day.",
@@ -30,4 +35,64 @@ function getNewRandomQuote(lastSentQuote = getCacheValue("lastSentQuote") || "")
   return randomQuote;
 }
 
-module.exports = { updateCache, getCacheValue, getNewRandomQuote, cache };
+function getEmailHtmlTemplateAndUpdate() {
+  // Email Template
+  const folderName = "email-html-template";
+  const emailTemplatePath = path.join(folderName, "email-template.html");
+  let emailTemplate = fs.readFileSync(emailTemplatePath, "utf8");
+
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const currentDay = daysOfWeek[new Date().getDay()];
+  const themes = {
+    Monday: {
+      gradient: "linear-gradient(135deg, #FF0000, #FFD700)", // Red to gold
+      themeMessage: "Motivational Start!",
+    },
+    Tuesday: {
+      gradient: "linear-gradient(135deg, #6DD5FA, #2980B9, #1E90FF)", // Sky blue, deep blue
+      themeMessage: "Energizing Vibes!",
+    },
+    Wednesday: {
+      gradient: "linear-gradient(135deg, #000000, #FFFFFF)", // Black to white
+      themeMessage: "Midweek Boost!",
+    },
+    Thursday: {
+      gradient: "linear-gradient(135deg, #FFA500, #FFD700, #FFFF00)", // Pure orange, pure golden, pure yellow
+      themeMessage: "Positive Momentum!",
+    },
+    Friday: {
+      gradient: "linear-gradient(135deg, #FF5722, #FF9800, #FFC107)", // Vibrant oranges and yellow
+      themeMessage: "Finish Strong!",
+    },
+    Saturday: {
+      gradient: "linear-gradient(135deg, #20B2AA, #3CB371, #2E8B57)", // Teal, medium green
+      themeMessage: "Relax & Reflect!",
+    },
+    Sunday: {
+      gradient: "linear-gradient(135deg, #FFD700, #FFC107, #FFB600)", // Gold, golden yellow
+      themeMessage: "Prepare & Plan!",
+    },
+  };
+  const { gradient, themeMessage } = themes["Monday"];
+  emailTemplate = emailTemplate.replaceAll("header_footer_gradient", gradient);
+  emailTemplate = emailTemplate.replace("${{currentDay}}", currentDay);
+  emailTemplate = emailTemplate.replace("${{themeMessage}}", themeMessage);
+
+  return emailTemplate;
+}
+
+module.exports = {
+  cache,
+  updateCache,
+  getCacheValue,
+  getNewRandomQuote,
+  getEmailHtmlTemplateAndUpdate,
+};
--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
-const fs = require("fs");
-const path = require("path");
 require("dotenv").config();
 
 // file imports
 const {
+  cache,
   updateCache,
   getNewRandomQuote,
-  cache,
+  getEmailHtmlTemplateAndUpdate,
 } = require("./helper/shared-data");
 const { generateRandomMessageID } = require("./helper/util");
 const { createTransporter } = require("./config/email-config");
@@ -16,18 +15,18 @@ const { createTransporter } = require("./config/email-config");
 // Express app
 const app = express();
 const port = process.env.PORT || 3000;
-
 // Email Configuration
 const transporter = createTransporter();
 
-// Email Template
-const folderName = "email-html-template";
-const emailTemplatePath = path.join(folderName, "email-template.html");
-const emailTemplate = fs.readFileSync(emailTemplatePath, "utf8");
-
 // Default Message for root URL
 app.get("/", (req, res) => {
-  res.send(`Hit The Send Email Endpoint to receive the email!`);
+  const domain = req.protocol + "://" + req.get("host");
+  const endpointLink = `${domain}/send-email`;
+
+  res.send(`
+    Hit The Send Email Endpoint to receive the email!<br>
+    <a href="${endpointLink}">Send Email</a>
+  `);
 });
 
 // Example Route
@@ -45,7 +44,7 @@ app.get("/send-email", (req, res) => {
     from: `Eureka! ${process.env.FROM_USER}`,
     to: `Priyanshu ${process.env.TO_USER}`, // Replace with your email
     // subject: `Your Morning Routine: ${randomQuote} - ${new Date().toLocaleTimeString()}`,
-    // html: emailTemplate,
+    // html: getEmailHtmlTemplateAndUpdate,
     headers: {
       "Content-Type": "text/html",
       "In-Reply-To": "",
@@ -54,14 +53,14 @@ app.get("/send-email", (req, res) => {
     },
   };
 
+  //dynamic mail options
+  mailOptions.subject = `Your Morning Routine: ${randomQuote} - ${new Date().toLocaleTimeString()}`;
+  mailOptions.html = getEmailHtmlTemplateAndUpdate();
+
   // Remove the cached value
   // TODO: it's not empacting anyway if I delete the lastSentQuote but, I'm keeping it to understand it later!
   //! and done this changes to update the value where it is resolved no any special changes, but want to understand more of it :) later!
   cache.delete("lastSentQuote");
-
-  //dynamic mail options
-  mailOptions.subject = `Your Morning Routine: ${randomQuote} - ${new Date().toLocaleTimeString()}`;
-  mailOptions.html = emailTemplate;
 
   // Send email
   transporter.sendMail(mailOptions, (error, info) => {
